fe4629df48d95ed669a650d1a04c45172d72cce1
BlackPunk <blackpunk1337@gmail.com>
2019-11-13 22:14:27 +0700

    #1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     # string = '(p and q ifg(r or s)'
     string = input('Input : ')
 
-    tokens = lexer.Lexer(string).tokenisasi()
+    tokens = lexer.Lexer(string.lower()).tokenisasi()
 
     print('Output : '+' '.join([str(token) for token in tokens]))
 main()
