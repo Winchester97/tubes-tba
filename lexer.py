bc57bb42198dffd4270518c5bf3468ab19a9921b
BlackPunk <blackpunk1337@gmail.com>
2019-11-13 23:37:19 +0700

    #ganti jadi lambda function

--- lexer.py
+++ lexer.py
@@ -7,35 +7,24 @@ class Lexer(object):
     def tokenisasi(self):
 
         state_pqrs = lambda string : 'error' if (bool(string)) else 1
-
         state_n = lambda string : state_no(string[1:]) if (string[0]=='o') else 'error'
         state_no = lambda string : state_not(string[1:]) if (string[0]=='t') else 'error'
         state_not = lambda string : 'error' if (bool(string)) else 2
-
         state_a = lambda string : state_an(string[1:]) if (string[0]=='n') else 'error'
         state_an = lambda string : state_and(string[1:]) if (string[0]=='d') else 'error'
         state_and = lambda string : 'error' if (bool(string)) else 3
-
         state_o = lambda string : state_or(string[1:]) if (string[0]=='r') else 'error'
         state_or = lambda string : 'error' if (bool(string)) else 4
-
         state_x = lambda string : state_xo(string[1:]) if (string[0]=='o') else 'error'
         state_xo = lambda string : state_xor(string[1:]) if (string[0]=='r') else 'error'
         state_xor = lambda string : 'error' if (bool(string)) else 5
-
         state_i = lambda string : state_if(string[1:]) if (string[0]=='f') else 'error'
-        def state_if(string):
-            if bool(string):
-                if(string[0] == 'f'): return state_iff(string[1:])
-                else: return 'error'
-            else: return 6
+        state_if = lambda string : (lambda string : state_iff(string[1:]) if (string[0]=='f') else 'error') if (bool(string)) else 6
         state_iff = lambda string : 'error' if (bool(string)) else 8
-
         state_t = lambda string : state_th(string[1:]) if (string[0]=='h') else 'error'
         state_th = lambda string : state_the(string[1:]) if (string[0]=='e') else 'error'
         state_the = lambda string : state_then(string[1:]) if (string[0]=='n') else 'error'
         state_then = lambda string : 'error' if (bool(string)) else 7
-
         state_bukakurung = lambda : 9
         state_tutupkurung = lambda : 10
 
