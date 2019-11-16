52ada2aab298691db74df134cdaeed3bec8881c7
BlackPunk <blackpunk1337@gmail.com>
2019-11-16 19:22:36 +0700

    # main mod

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from lexer import tokenisasi
 
-
 def main():
     while True:
         string = input('Input : ').lower().strip()
         tokens = tokenisasi(string)
         print('Output (Token Lexic) : {0}'.format(' '.join([str(token) for token in tokens])))
+
 main()
